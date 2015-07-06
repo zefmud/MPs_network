d750d0a41c1aa0a6505258fd313b83faa3b0e41a
zefmud <paul.myronov@gmail.com>
2015-07-06 18:22:38 +0300

    Draft laws list added

--- app.R
+++ app.R
@@ -1,10 +1,15 @@
 library(shiny)
 library(networkD3)
+load("data/all_nodes.Rda")
+load("data/factions.Rda")
+load("data/graph.Rda")
+load("data/partners.Rda")
+partners$name <- as.character(partners$name)
+f <- unique(as.character(factions$faction_title))
+
 #server function
-server <- function(input, output) {
-  load("data/all_nodes.Rda")
-  load("data/factions.Rda")
-  load("data/graph.Rda")
+server <- function(input, output, session) {
+
   draw_graph <- function(graph, min_value, f = unique(factions$faction_title))
   {
     A <- graph[graph$value >= min_value, ]
@@ -26,29 +31,50 @@ server <- function(input, output) {
     colourScale = JS("d3.scale.category10()"),
 		opacity = 0.9, zoom = FALSE, legend = TRUE)
   }
+  
+    
+  update_second_droplist <-  function() {
+    selected <- which(partners$name == input$chosen_MP)
+    partners_names <- as.character(partners$partners[[selected]]$name)
+    updateSelectInput(session, inputId = "MP2_selectInput", choices = partners_names[order(partners_names)], selected = partners_names[1])
+  }
+  
+  get_bills_list <- function(d1, d2)
+  {
+    d1 <- all_nodes$MP_ID[all_nodes$name == d1]
+    d2 <- all_nodes$MP_ID[all_nodes$name == d2]
+    if (sum(((graph$source == d1)|(graph$source == d2))&((graph$target == d1)|(graph$target == d2))) == 0)
+    {
+      update_second_droplist()
+      NULL
+    } else {
+    
+      bills_list_number <- which(((graph$source == d1)|(graph$source == d2))&((graph$target == d1)|(graph$target == d2)))
+      bills <- graph$bills[[bills_list_number]]
+      bills$bill_title <- paste("<a href = '", bills$bill_link,"'>",bills$bill_title,"</a>", sep="")
+      bills$bill_link <- NULL
+      bills$type <- NULL
+      #bills$date <- format(bills$date, format = "%d.%m.%Y")
+      names(bills) <- c ("Номер", "Дата реєстрації", "Назва", "Став чинним актом")
+      bills
+    }
+  }
+  
+  observe({
+    if (input$MP2_selectInput != '') 
+    {
+      d1 <- input$chosen_MP
+      d2 <- input$MP2_selectInput
+      t <- get_bills_list(d1, d2)
+       output$bills_table <- DT::renderDataTable(t, options = list(language = list(url = "//cdn.datatables.net/plug-ins/1.10.7/i18n/Ukranian.json")),
+                                                 caption = paste(d1, " та ", d2, ": cпільні законопроекти", sep = ""), escape = F, rownames = NULL)
+    } else {
+      update_second_droplist()
+    }
+  })
+  
   draw_ind_graph <- function(d, level, f = unique(factions$faction_title))
   {
-    #   path <- numeric(0)
-    #   find_partners <- function(d, level)
-    #   {
-    #     path <<- c(path, d)
-    #     if (level == 0)
-    #     {
-    #       d
-    #     } else {
-    #       new_nodes <- c(graph$target[(graph$source == d) & !(graph$target %in% path)],
-    #                      graph$source[(graph$target == d) & !(graph$source %in% path)])
-    #       ret <- d
-    #       for (i in 1:length(new_nodes))
-    #       {
-    #         ret <- c(ret, find_partners(new_nodes[i], level - 1))
-    #       }
-    #       ret
-    #     }
-    #   }
-    #   nodes <- find_partners(d, level)
-    #   nodes <- all_nodes[all_nodes$MP_ID %in% nodes, ]
-    #   A <- graph[(graph$source %in% nodes$MP_ID) & (graph$target %in% nodes$MP_ID), ]
     A <- graph[(graph$source == d)| (graph$target == d),]
     if (length(A$source) > 0)
     {
@@ -92,13 +118,14 @@ server <- function(input, output) {
     }
     #forming an expression to calculate distance
     max_value <- max(A$value)
-    dist_expr <- paste('function(d){return 25 +  (1 / d.value) * 200}')
+    dist_expr <- paste('function(d){return 25 +  (1 / d.value) * 150}')
     forceNetwork(Links = A, Nodes = nodes, Source = "source", Target = "target", 
                  Value = "value", NodeID = "name", Nodesize = "size", Group = "group",  
                  colourScale = JS("d3.scale.category10()"), linkDistance = JS(dist_expr), 
                  opacity = 1, zoom = FALSE, legend = TRUE)
     
   }
+  
   get_MP_ID <- reactive({
     all_nodes$MP_ID[all_nodes$name == input$chosen_MP]
   })
@@ -117,14 +144,13 @@ server <- function(input, output) {
 #   })
 }
 #ui function
-load("data/factions.Rda")
-load("data/all_nodes.Rda")
-f <- unique(as.character(factions$faction_title))
+
+
 ui <- shinyUI(fluidPage(
   tabsetPanel(
     tabPanel("Усі депутати",
       sidebarLayout(
-        sidebarPanel(
+        sidebarPanel(width = 3, 
           h3("Депутатські групи"), 
           helpText("Хто найтісніше співпрацює всередині кожної фракції та між ними?"),
           checkboxGroupInput("factions", 
@@ -144,14 +170,15 @@ ui <- shinyUI(fluidPage(
                    Товшина лінії зв’язку залежить від кількості законопроектів, 
                    які депутати ініціювали разом.")),
         
-        mainPanel(forceNetworkOutput("graph", height="600px"))
+        mainPanel(forceNetworkOutput("graph", height="500px"))
       )
     ),
     tabPanel("Конкретний депутат",
       sidebarLayout(
-        sidebarPanel(
-          h3("Кола партнерів окремого депутата"),
-          helpText("Побачите усіх, з ким депутат подав хоча б один спільний законопроект"),
+        sidebarPanel(width = 3, 
+          h4("Партнери окремого депутата"),
+          helpText("Побачите усіх, з ким депутат подав хоча б один спільний законопроект. Чим більша відстань і менший кружечок депутатів - тим менш 		законопроектів він спільно ініціював з обраним нардепом. Наведіть мишкою на кружечок, і після імені депутата 
+                   побачите точну кількість спільних законопроектів"),
           selectInput("chosen_MP", "Оберіть депутата:", 
                       choices = all_nodes$name[order(all_nodes$name)]),
           checkboxGroupInput("factions_ind", 
@@ -162,12 +189,14 @@ ui <- shinyUI(fluidPage(
 #                    actionButton("select_all_ind", "Прибрати всі")
 #             )
 #           ),
-          helpText("Чим більша відстань і менший кружечок депутатів - тим менше законопроектів він
-                   спільно ініціював з обраним нардепом. Наведіть мишкою на кружечок, і після імені депутата 
-                   побачите точну кількість спільних законопроектів.")
+	  selectInput(inputId = "MP2_selectInput", label = "Вивести список законопроектів", choices = NULL),
+          helpText("Ви також можете подивитися список усіх законопроектів, які двоє депутатів ініціювали разом. Для цього потрібно обрати другого 		депутата зі списку партнерів першого")
           #sliderInput("level", "Оберіть глибину пошуку:", min = 1, max = 5, value = 1)
         ),
-        mainPanel(forceNetworkOutput("ind_graph", height="600px"))
+        mainPanel(
+	  forceNetworkOutput("ind_graph", height="500px"),
+	  DT::dataTableOutput('bills_table')
+	)
       )
     )
   ), title = "ЧЕСНО::Графи"
