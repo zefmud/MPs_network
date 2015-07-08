cd39438c2b8d392ee3a90d4e2046002148115672
zefmud <paul.myronov@gmail.com>
2015-07-08 11:18:32 +0300

    Possible error with bills list corrected

--- app.R
+++ app.R
@@ -86,6 +86,7 @@ server <- function(input, output, session) {
     number_all <- all_nodes$size[all_nodes$name == d1]
     output$table_header1 <- renderUI({h5(paste(d1, " всього подав(-ла) ", number_all, " законопроект(-и,-ів)."), sep="")})
     output$help_second_list <-renderUI({helpText(paste("Депутати в другому списку відсортовані за кількістю законопроектів, яку ", d1, " подав(-ла) у співпраці із ними.", sep = "")) })
+    update_table()
   })
   observeEvent(input$MP2_selectInput, {
     update_table()
